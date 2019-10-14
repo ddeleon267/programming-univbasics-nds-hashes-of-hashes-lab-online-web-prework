75fd65a6eb6ace65b69800eec7bf43a2d8df9d32
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-14 12:37:08 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,7 +42,7 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
   Hash.new
-  PORTION_3[:sub_category][:sub_category][:sub_category][:label]
+  return PORTION_3[:sub_category][:sub_category][:sub_category][:label]
   binding.pry
 
 
